de6fee23aa0402a1da55cb8df6410d308f266b79
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2017-07-03 15:08:21 -0500

    rows -> services, promotions, summer beauty and small fixes

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,7 @@
 $(document).ready(() => {
+  servicesHeight();
+  $('.tooltipped').tooltip({delay: 50});
+
   $('.timepicker').pickatime({
     default: 'now', // Set default time
     fromnow: 0,       // set default time to * milliseconds from now (using with default = 'now')
@@ -10,8 +13,20 @@ $(document).ready(() => {
     ampmclickable: true, // make AM PM clickable
     aftershow: function(){} //Function for after opening timepicker
   });
+
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
     selectYears: 15 // Creates a dropdown of 15 years to control year
   });
 });
+
+function servicesHeight() {
+  const heights = [];
+  const cards = $('.services-card');
+
+  for (var i = 0; i < cards.length; i++) {
+    heights.push($(cards[i]).height());
+  }
+  $(cards).height(Math.max(...heights));
+  console.log(heights);
+}
