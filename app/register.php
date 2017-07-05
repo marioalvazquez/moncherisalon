136951d94b1b84a40782b35c2fdb191b7ff409fe
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2017-07-05 14:38:41 -0500

    New version of email sender

--- app/register.php
+++ app/register.php
@@ -13,34 +13,32 @@
   $resultado = $connect->query($query);
 
   if ($resultado) {
+    $header = "From: MonCheri <contacto@moncheribeautysalon.com>\r\n";
+    $header .= "MIME-Version: 1.0\r\n";
+    $header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+
+      mail("marioal.vazquez@gmail.com",
+       "Nueva agenda para Mon Cheri",
+       "Nombre: $name.\n Día: $date a las $time\n Servicios: $services\n Teléfono: $phone\n Correo: $mail", $header);
+
+    $message = '<!DOCTYPE html><html><head><style>';
+    $message .= '.container{margin:2rem;padding:2rem;background:#9c27b0;';
+    $message .= 'color:#e1bee7;}h2{text-align:center; font-size:20px;color: #FFF}';
+    $message .= '</style></head>';
+    $message .= '<body><div class="container"><h2>Agendamos exitosamente tu cita';
+    $message .= 'en Mon Cheri</h2>'
+    $message .= "<p>Fecha: $date, Hora: $time</p>";
+    $message .= "<p>Servicios: $services</p>";
+    $message .= "<p>No olvides presentarte 10 minutos antes de tu cita.</p>";
+    $message .= "<p>¡Saludos!</p>";
+    $message .= '</div></body></html>';
+
+       mail($mail, "Registro de cita exitoso", $message, $header);
     echo "1";
   }
   else{
     echo "0";
   }
-
-$header = "From: MonCheri <contacto@moncheribeautysalon.com>\r\n";
-$header .= "MIME-Version: 1.0\r\n";
-$header .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-
-  mail("marioal.vazquez@gmail.com",
-   "Nueva agenda para Mon Cheri",
-   "Nombre: $name.\n Día: $date a las $time\n Servicios: $services\n Teléfono: $phone\n Correo: $mail", $header);
-
-$message = '<!DOCTYPE html><html><head><style>';
-$message .= '.container{margin:2rem;padding:2rem;background:#9c27b0;';
-$message .= 'box-shadow: 0 10px 20px rgba(0,0,0,0.19);';
-$message .= 'color:#e1bee7;}h2{text-align:center; font-size:20px;color: #FFF}';
-$message .= '</style></head>';
-$message .= '<body><div class="container"><h2>Agendamos exitosamente tu cita';
-$message .= 'en Mon Cheri</h2>'
-$message .= "<p>Fecha: $date, Hora: $time</p>";
-$message .= "<p>Servicios: $services</p>";
-$message .= "<p>No olvides presentarte 10 minutos antes de tu cita.</p>";
-$message .= "<p>¡Saludos!</p>";
-$message .= '</div></body></html>';
-
-   mail($mail, "Registro de cita exitoso", $message, $header);
 
 
  ?>
