aa382122159d59374f761cf7d62f3a684fadf903
Mario Alberto Vàzquez Lòpez <marioal.vazquez@gmail.com>
2017-07-05 14:09:46 -0500

    New message for customer

--- app/register.php
+++ app/register.php
@@ -19,11 +19,24 @@
     echo "0";
   }
 
-$header = "From: MonCheri <contacto@moncheri.com>\r\n";
+$header = "From: MonCheri <contacto@moncheribeautysalon.com>\r\n";
 
   mail("marioal.vazquez@gmail.com",
    "Nueva agenda para Mon Cheri",
    "Nombre: $name.\n Día: $date a las $time\n Servicios: $services\n Teléfono: $phone\n Correo: $mail", $header);
+
+$message = '<DOCTYPE html><html><head><style>';
+$message .= '.container{margin:2rem;padding:2rem;text-align:center;';
+$message .= 'border-radius:10px; border: 1px solid #e1bee7}';
+$message .= '</style></head>';
+$message .= '<body><div class="container"><h2>Agendamos exitosamente tu cita</h2>';
+$message .= "<p>Fecha: $date, Hora: $time</p>";
+$message .= "<p>Servicios: $services</p>";
+$message .= "<p>No olvides presentarte 10 minutos antes de tu cita.</p>";
+$message .= "<p>¡Saludos!</p>";
+$message .= '</div></body></html>';
+
+   mail($mail, "Registro de cita exitoso", $message, $header);
 
 
  ?>
